1da11b055e97ceeaae59e3605df2148a0c2d0136
locdp <locdp@suga.com.vn>
2024-09-13 02:19:55 +0700

    feat: mirgate to bootstrap 5

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AdminLayout from './layouts/Admin'
+import './assets/plugins/nucleo/css/nucleo.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import './assets/scss/argon-dashboard-react.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/admin/*" element={<AdminLayout />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
