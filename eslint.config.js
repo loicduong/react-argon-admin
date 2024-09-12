5c534b286c371d71799633e223bdbc405738fa11
locdp <locdp@suga.com.vn>
2024-09-12 23:53:42 +0700

    chore: update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,15 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import antfu from '@antfu/eslint-config'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default antfu(
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    formatters: true,
+    regexp: false,
+    unocss: false,
+    react: true,
+  },
+  {
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ecmaVersion: 'latest',
     },
   },
 )
